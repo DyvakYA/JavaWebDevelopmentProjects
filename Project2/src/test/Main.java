234fa1d19a1db870d4d1ec8b1aadcdbd3e7449e6
DyvakYA <dyvakyurii@gmail.com>
2016-11-24 01:05:45 +0200

    new version

--- Project2/src/test/Main.java
+++ Project2/src/test/Main.java
@@ -1,8 +1,10 @@
-package main;
+package test;
+
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Initialization
         Model model = new Model();
         View view = new View();
@@ -10,5 +12,4 @@ public class Main {
         // Run
         controller.processUser();
     }
-
 }
--- Project2/src/test/Model.java
+++ Project2/src/test/Model.java
@@ -1,4 +1,4 @@
-package main;
+package test;
 
 /**
  * Created by User on 17.03.2016.
